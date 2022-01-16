138bbbc34cb876b40137b598a4e1295722804993
Aymenbs22 <aymenbenslima1@gmail.com>
2022-01-16 10:00:26 -0800

    opcode pop removes the top element of the stack

---
+++ pop.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include "monty.h"
+/**
+ * pop - pops the very top
+ * @stack: stack 
+ * @line_cnt: line count
+ * Return: 0
+*/
+void pop(stack_t **stack, unsigned int line_cnt)
+{
+	stack_t *tmp = NULL;
+
+	if (!stack || !*stack)
+{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_cnt);
+		status = EXIT_FAILURE;
+		return;
+}
+
+	tmp = (*stack)->next;
+	free(*stack);
+	*stack = tmp;
+	if (!*stack)
+		return;
+	(*stack)->prev = NULL;
+}
