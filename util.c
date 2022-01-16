48de8be920cb920acd525ac4065e976cb29a7ba1
Aymenbs22 <aymenbenslima1@gmail.com>
2022-01-16 10:05:27 -0800

    util

---
+++ util.c
@@ -0,0 +1,40 @@
+#include "monty.h"
+#include <ctype.h>
+/**
+ * is_digit - checks if a string is a digit
+ * @string: string
+ *
+ * Return: 1 or 0
+ */
+int is_digit(char *string)
+{
+	if (!string || *string == '\0')
+		return (0);
+	if (*string == '-')
+		string++;
+	while (*string)
+{
+	if (isdigit(*string) == 0)
+		return (0);
+	string++;
+}
+	return (1);
+}
+/**
+ * isnumber - checks if a string is a number
+ * @str: string
+ * Return: 1 or 0
+*/
+int isnumber(char *str)
+{
+	int i;
+
+	if (!str)
+		return (0);
+
+  	for (i = 0; str[i]; i++)
+		if (i < '0' || i > '9')
+			return (0);
+
+	return (1);
+}
