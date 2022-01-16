2df52bec16969c20e7000889aa2a4edab4bf4d28
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-16 19:23:19 +0100

    first try

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - nada
+ * @stack: head of stack
+ * @line_cnt : line counter
+ */
+void nop(stack_t **stack, unsigned int line_cnt)
+{
+	(void) stack;
+	(void) line_cnt;
+}
+
